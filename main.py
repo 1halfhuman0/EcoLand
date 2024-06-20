77a0e6b57fa145a1268649e7e33ac3903636d88f
1halfhuman0 <1halfhuman0@gmail.com>
2024-06-20 16:35:27 +0900

    Feat : Creature가 이제 배회함

--- main.py
+++ main.py
@@ -28,22 +28,34 @@ class Creature:
         self.x = x
         self.y = y
         self.size = 20
-        self.speed = 5
+        self.speed = 2
+        self.change_direction_interval = 60  # 방향을 바꾸는 간격 (프레임 단위)
+        self.change_direction_timer = self.change_direction_interval
+        self.direction = (random.choice([-1, 0, 1]), random.choice([-1, 0, 1]))  # 초기 방향 설정
 
     def move(self):
-        self.x += random.choice([-1, 0, 1]) * self.speed
-        self.y += random.choice([-1, 0, 1]) * self.speed
+        self.change_direction_timer -= 1
+        if self.change_direction_timer <= 0:
+            self.direction = (random.choice([-1, 0, 1]), random.choice([-1, 0, 1]))  # 새로운 방향 설정
+            self.change_direction_timer = self.change_direction_interval  # 타이머 재설정
+
+        self.x += self.direction[0] * self.speed
+        self.y += self.direction[1] * self.speed
 
         # 화면 경계 조건 설정
         if self.x < 0:
             self.x = 0
+            self.direction = (1, self.direction[1])  # 화면 경계에 닿으면 방향 전환
         elif self.x > screen_width - self.size:
             self.x = screen_width - self.size
+            self.direction = (-1, self.direction[1])  # 화면 경계에 닿으면 방향 전환
 
         if self.y < 0:
             self.y = 0
+            self.direction = (self.direction[0], 1)  # 화면 경계에 닿으면 방향 전환
         elif self.y > screen_height - self.size:
             self.y = screen_height - self.size
+            self.direction = (self.direction[0], -1)  # 화면 경계에 닿으면 방향 전환
 
     def draw(self, screen):
         pygame.draw.rect(screen, CREATURE_COLOR, (self.x, self.y, self.size, self.size))
